a3caa82d0259a51abf5732492b0077e1bf8eee30
Diana Gouveia <75624369+dianacpg@users.noreply.github.com>
2020-12-17 15:26:41 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var _ = require('lodash');
+
+var array = [ 1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3))
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var deletebtn = document.querySelector (".close");
+
+function inputLength () {
+  return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    var removeBtn= document.createElement("button");
+    removeBtn.appendChild(document.createTextNode("Delete"));
+    removeBtn.className = "close";
+    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(removeBtn);
+    ul.appendChild(li);
+    input.value="";
+
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+    createListElement();
+  }
+}
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+function doneLine(){
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle ("done");
+  }
+}
+
+function removeItem(){
+  if (event.target.className === "close") {
+    event.target.parentElement.remove();
+  }
+}
+
+ul.addEventListener("click", doneLine)
+button.addEventListener("click", addListAfterClick)
+input.addEventListener("keypress", addListAfterKeypress)
+ul.addEventListener("click", removeItem)
+
+
+
+
+
